e6727cbe15ab2299e2203e67ba258aece6b525ca
deepu <deepaktekchandani143@gmail.com>
2022-03-10 17:56:36 +0500

    Changing in controllar and Create EmailService.java file...

--- src/main/java/com/email/com/email/services/EmailService.java
+++ src/main/java/com/email/com/email/services/EmailService.java
@@ -1,4 +1,27 @@
 package com.email.com.email.services;
 
+import org.springframework.stereotype.Service;
+
+import java.util.Properties;
+
+@Service
 public class EmailService {
+
+    public void sendEmail(String to,String subject,String message){
+
+        //variable for gmail
+        String host = "smtp.gmail.com";
+
+        //get the system properties
+        Properties properties= System.getProperties();
+        System.out.println("Properties"+properties);
+
+        //setting important information to  properties object
+
+        //host set
+        properties.put("mail.smtp.host",host);
+        properties.put("mail.smtp.port","456");
+        properties.put("mail.smtp.sslenable","true");
+        properties.put("mail.smtp.")
+    }
 }
